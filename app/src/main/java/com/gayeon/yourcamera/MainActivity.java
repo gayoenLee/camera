55a8905f6a9a587fe37c1d6b1d2f3c7cbadaba2d
gayeon <leeedho67@gmail.com>
2020-04-11 14:23:53 +0900

    얼굴 위에성 크기에 맞게 수염 올리기 자바, xml코드 작성

--- app/src/main/java/com/gayeon/yourcamera/MainActivity.java
+++ app/src/main/java/com/gayeon/yourcamera/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity
     ImageButton takePhotoBtn;
     ImageButton changeDirectionBtn;
     ImageButton makeFilterBtn;
-    ImageButton videoBtn;
+    ImageButton overlayBtn;
     String filename;
     Button nothingBtn;
     Button hsvBtn;
@@ -125,7 +125,10 @@ public class MainActivity extends AppCompatActivity
     Mat roi_gray;
     Mat roi_rgb;
     Rect roi;
-    //private String  glassesImage = "glasses.png";
+    Button glassesBtn;
+    Button mustacheBtn;
+    boolean glassesFilter = false;
+    boolean mustacheFilter = false;
 
     private Mat glasses;
     private Mat mustache;
@@ -164,7 +167,7 @@ public class MainActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        videoBtn = findViewById(R.id.video_record_btn);
+        overlayBtn = findViewById(R.id.overlay_image_btn);
         changeDirectionBtn = findViewById(R.id.change_direction_btn);
         makeFilterBtn = findViewById(R.id.magic_effect_btn);
         nothingBtn = findViewById(R.id.no_effect);
@@ -173,6 +176,8 @@ public class MainActivity extends AppCompatActivity
         grayBtn = findViewById(R.id.gray_effect);
         roiBtn = findViewById(R.id.roi_effect);
         testImage = findViewById(R.id.image);
+        glassesBtn = findViewById(R.id.glasses);
+        mustacheBtn = findViewById(R.id.mustache);
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this,
@@ -237,15 +242,40 @@ public class MainActivity extends AppCompatActivity
 
         Log.i(TAG, "얼굴과 눈 검출하기 위해 학습시켜 놓은 분류기 로드");
         //녹화 기능
-        videoBtn.setOnClickListener(new View.OnClickListener() {
+        overlayBtn.setOnClickListener(new View.OnClickListener() {
                                         @Override
                                         public void onClick(View v) {
                                             Log.i(TAG, "얼굴 인식 시작 버튼 클릭");
                                             FACEDETECT = true;
+                                            if (glassesBtn.getVisibility() == v.GONE) {
+                                                Log.i(TAG, "필터 안에 버튼 보이도록 하기");
+                                                glassesBtn.setVisibility(v.VISIBLE);
+                                            } else {
+                                                glassesBtn.setVisibility(v.GONE);
+                                            }
+                                            if (mustacheBtn.getVisibility() == v.GONE) {
+                                                mustacheBtn.setVisibility(v.VISIBLE);
+                                            } else {
+                                                mustacheBtn.setVisibility(v.GONE);
+                                            }
 
                                         }
                                     }
         );
+         glassesBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 glassesFilter = true;
+             }
+         });
+         mustacheBtn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 mustacheFilter = true;
+
+
+             }
+         });
         //필터 선택 버튼
         makeFilterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -437,7 +467,13 @@ public class MainActivity extends AppCompatActivity
             //타원 그리기
             // ellipse(matInput, centre1, new Size(facesArray[i].width * 0.5, facesArray[i].height * 0.5), 0, 0, 360,
             //        new Scalar(255, 0, 255), 4, 8, 0);
-            matInput = putMask(matInput, centre1, new Size(facesArray[i].width*0.8, facesArray[i].height*0.6));
+             if(glassesFilter){
+                 matInput = putMask(matInput, centre1, new Size(facesArray[i].width*0.8, facesArray[i].height*0.6));
+             }
+             if(mustacheFilter){
+
+                 matInput = putMustache(matInput, new Point(facesArray[i].x+facesArray[i].width*0.5, facesArray[i].y+facesArray[i].height*0.5+60), new Size(facesArray[i].width+0.4, facesArray[i].height*0.6));
+             }
 
            // matInput = putMask(glasses, centre1, new Size(facesArray[i].width * 0.5, facesArray[i].height * 0.5));
 //그레이 이미지에서 faceArray의 i번째 얼굴을 잘라낸다.
@@ -648,6 +684,82 @@ public class MainActivity extends AppCompatActivity
         //이 값을 이용해 그레이 영상에서 배경과 물체를 분리해냄.
         //그레이스케일 이미지, 픽셀 문턱값, 픽셀 문턱값보다 클 때 적용되는 최대값, 문턱값 적용 방법 또는 스타일
         Imgproc.threshold(mask_grey, mask_grey, 50, 10, Imgproc.THRESH_TOZERO);
+
+        //마스크 채널 4개 배열 선언(rgba)
+        //결과 마스크 4개 배열 만들어서 각각 mat넣음 만듦.
+        ArrayList<Mat> maskChannels = new ArrayList<>(4);
+        Log.i(TAG, " 마스크 채널 확인 : "+ maskChannels.size());
+        ArrayList<Mat> result_mask = new ArrayList<>(4);
+        result_mask.add(new Mat());
+        result_mask.add(new Mat());
+        result_mask.add(new Mat());
+        result_mask.add(new Mat());
+
+        //4채널을 가진 mask_resized ; 원래는 3채널인 영상을 3개의 1채널 영상으로 분리해줌.(B, G, R)각각의 싱글 채널을 maskchannels에 닮음.
+        Core.split(mask_resized, maskChannels);
+        //OpenCV 함수: bitwise_and(이미지1, 이미지2, 결과, 마스크)..이미지2가 흑백이미지일경우 이미지2의 흰색부분만 출력
+        //비트연산. 이미지에서 특정 영역을 추출할 때 유용하게 사용.
+        //mask가 검정색이 아닌 경우만 통과가 되기때문에 mask영역 이외는 모두 제거됨.
+        //(inputarray src1, inputarray src2, outputarray dst, inputarray mask = noarray())..mask범위 내에서 두 개의 어레이(src1, src2)의 비트연산and결과
+        Core.bitwise_and(maskChannels.get(0),mask_grey,result_mask.get(0));
+        Core.bitwise_and(maskChannels.get(1),mask_grey,result_mask.get(1));
+        Core.bitwise_and(maskChannels.get(2),mask_grey,result_mask.get(2));
+        Core.bitwise_and(maskChannels.get(3),mask_grey,result_mask.get(3));
+
+        //분리됐던 3개의 채널을 다시 하나의 3채널 컬러 영상으로 생성.
+        Core.merge(result_mask, roi_gray);
+
+        //not은 색이 반대로 나타나는 것.
+        Core.bitwise_not(mask_grey, mask_grey);
+
+        //4개의 채널을 담을 배열 선언.
+        ArrayList<Mat> srcChannels = new ArrayList<>(4);
+        //4개 채널 가진 src_roi를 split해서 각 싱글 채널을 소스 채널에 담음.
+        Core.split(src_roi, srcChannels);
+
+        Core.bitwise_and(srcChannels.get(0), mask_grey, result_mask.get(0));
+        Core.bitwise_and(srcChannels.get(1), mask_grey, result_mask.get(1));
+        Core.bitwise_and(srcChannels.get(2), mask_grey, result_mask.get(2));
+        Core.bitwise_and(srcChannels.get(3), mask_grey, result_mask.get(3));
+
+        Core.merge(result_mask, roi_rgb);
+
+        Core.addWeighted(roi_gray, 1, roi_rgb, 1, 0, roi_rgb);
+//roi_rgb를 src의 roi영역에 붙인다.
+        roi_rgb.copyTo(new Mat(src, roi));
+        Log.d(TAG, "src 리턴" + src.width());
+
+        return src;
+    }
+    //src는 배경
+    public Mat putMustache(Mat src, Point center, Size face_size) {
+
+        //마스크 사이즈 조정
+        Mat mask_resized = new Mat();
+//미리보기 이후 쟈른 얼굴의 roi
+        src_roi = new Mat();
+        roi_gray = new Mat();
+        //원본 이미지, 결과 이미지 크기, 줄일 사이즈
+        Log.d(TAG, "resize 호출 전" + mask_resized.empty());
+        Imgproc.resize(mustache, mask_resized, face_size);
+        Log.d(TAG, "리사이즈 호출 후" + mask_resized.empty());
+
+        //효과가 그려질부분
+        roi = new Rect((int) (center.x - face_size.width / 2), (int) (center.y - face_size.height / 2), (int) face_size.width, (int) face_size.height);
+        //src에서 얼굴영억만큼 잘라서 src_roi에 붙인다.///src는 아마도 matInput과 같은 느낌'
+        Log.d(TAG, "roi 가로세로길이" + roi.height + "/" + roi.width);
+        src.submat(roi).copyTo(src_roi);
+
+        //검정색 부분 투명하게 만들기
+        Mat mask_grey = new Mat();
+        //선글라스 결과 이미지
+        roi_rgb = new Mat();
+        //검정색과 흰색으로 색칠. 넣어줄 이미지를 그레이 레벨로 읽어서 나중에 마스크로 사용.
+        //make binary images for masking background and foreground images
+        Imgproc.cvtColor(mask_resized, mask_grey, Imgproc.COLOR_BGRA2GRAY);
+        //이 값을 이용해 그레이 영상에서 배경과 물체를 분리해냄.
+        //그레이스케일 이미지, 픽셀 문턱값, 픽셀 문턱값보다 클 때 적용되는 최대값, 문턱값 적용 방법 또는 스타일
+        Imgproc.threshold(mask_grey, mask_grey, 250, 255, Imgproc.THRESH_BINARY_INV);
 
         //마스크 채널 4개 배열 선언(rgba)
         //결과 마스크 4개 배열 만들어서 각각 mat넣음 만듦.
